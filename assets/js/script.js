5846d78ba716b485039b16b85cf4ae6049c80ffb
NicoleHitter <nicole.hitter92@gmail.com>
2022-10-16 12:34:16 +0000

    Add media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,21 +45,14 @@ const startButton = document.getElementById('start-btn');
 const submitButton = document.getElementById('submit-btn');
 const nextButton = document.getElementById('next-btn');
 const questionArea = document.getElementById('text-container');
-const end = document.getElementById('end-of-page');
+const end = document.getElementById('end-of-quiz');
+const optionsButtons = document.querySelectorAll('.buttons');
+const checkBox = document.getElementsByName('planswer');
 
 let questionIndex = -1;
 let playerAnswer = '';
 let score = 0;
 let negscore = 0;
-
-//Wait for the DOM to finish loading before running the game 
-// Get the button elements and add event listeners to them
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded');
-    startButton.addEventListener('click', startQuiz);
-    submitButton.addEventListener('click', checkAnswer);
-    nextButton.addEventListener('click', displayQuestion);
-});
 
 /**
  * This function will show to console that quiz has started
@@ -68,9 +61,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
  */
 function startQuiz() {
     console.log('started');
-    startButton.classList.add('hide');
-    startButton.classList.add('hide');
     questionArea.classList.remove('hide');
+    startButton.classList.add('hide');
     displayQuestion();
 }
 
@@ -89,9 +81,15 @@ function displayQuestion() {
         document.getElementById('a').innerHTML = answersList.a;
         document.getElementById('b').innerHTML = answersList.b;
         document.getElementById('c').innerHTML = answersList.c;
+        // this loop will change the previous checked button to not checked 
+        for (let option = 0; option < checkBox.length; option++) {
+            checkBox[option].checked = false;
+        }
     } else {
         endQuiz();
     }
+    submitButton.classList.add('hide');
+    nextButton.classList.add('hide');
 }
 
 /**
@@ -118,10 +116,12 @@ function checkAnswer() {
         score++;
         document.getElementById('score').innerHTML = `Correct answers: ${score}`;
     } else {
-        document.getElementById('answermsg').innerHTML = `Incorrect. The correct answer is ${MY_QUESTIONS[questionIndex].correctAnswer}!`
+        document.getElementById('answermsg').innerHTML = `Incorrect. The correct answer is ${MY_QUESTIONS[questionIndex].correctAnswer}!`;
         negscore++;
         document.getElementById('negscore').innerHTML = `Wrong answers: ${negscore}`;
     }
+    nextButton.classList.remove('hide');
+    submitButton.classList.add('hide');
 }
 
 /**
@@ -130,9 +130,28 @@ function checkAnswer() {
  * restart button that if pressed will reload page at the beginning to play game again
  */
 function endQuiz() {
-    document.getElementById("final-message").textContent =
+    document.getElementById("final-message").innerHTML =
         `You have ${score} correct answer(s) and ${negscore} incorrect answer(s)!`;
     end.classList.remove('hide');
     questionArea.classList.add('hide');
-
 }
+
+//Wait for the DOM to finish loading before running the game 
+// Get the button elements and add event listeners to them
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded');
+    startButton.addEventListener('click', startQuiz);
+    submitButton.addEventListener('click', checkAnswer);
+    nextButton.addEventListener('click', displayQuestion);
+
+    optionsButtons.forEach(buttons => {
+        buttons.addEventListener('click', function handleClick(event) {
+            console.log('button clicked', event);
+
+            submitButton.classList.remove('hide');
+        });
+    });
+
+    submitButton.addEventListener('click', checkAnswer);
+    nextButton.addEventListener('click', displayQuestion);
+});
